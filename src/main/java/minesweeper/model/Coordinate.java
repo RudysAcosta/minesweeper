374b3e388d48a1550c250cf9c83856b99b153fa0
Rudys Acosta <ncrousset926@gmail.com>
2025-03-04 21:45:34 -0500

    Make Coordinate class

---
+++ src/main/java/minesweeper/model/Coordinate.java
@@ -0,0 +1,41 @@
+package minesweeper.model;
+
+public class Coordinate {
+
+    private int x;
+
+    private int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordinate(String coordinateString) {
+        if (coordinateString == null || !coordinateString.contains("-")) {
+            throw new IllegalArgumentException("Invalid format. It must be 'x-y'.");
+        }
+
+        String[] coordinates = coordinateString.split("-");
+
+        if (coordinates.length != 2) {
+            throw new IllegalArgumentException("Invalid format. It must be 'x-y'.");
+        }
+
+        try {
+            this.x = Integer.parseInt(coordinates[0].trim());
+            this.y = Integer.parseInt(coordinates[1].trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("The coordinates must be valid integer numbers.");
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
